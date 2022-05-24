5feddf382da0ad224caeb8b587093ac5824397aa
Anunig223 <79616960+Anunig223@users.noreply.github.com>
2022-05-23 17:58:35 -0700

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,55 @@
+
+var firebaseConfig = {
+      apiKey: "AIzaSyA5nWJHvL6-Kc1MPiaZJoRWQd5Cwqk4AAI",
+      authDomain: "kwitter-app-new.firebaseapp.com",
+      databaseURL: "https://kwitter-app-new-default-rtdb.firebaseio.com",
+      projectId: "kwitter-app-new",
+      storageBucket: "kwitter-app-new.appspot.com",
+      messagingSenderId: "550849866977",
+      appId: "1:550849866977:web:6cd07011b512da2ab61ba0"
+    };
+    
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+user_name = localStorage.getItem("user_name");
+document.getElementById("user_name").innerHTML = "Welcome " + user_name + "!";
+
+function addRoom() {
+      room_name = document.getElementById("room_name").value;
+      firebase.database().ref("/").child(room_name).update({
+            purpose: "adding room name"
+      });
+      localStorage.setItem("room_name", room_name);
+      window.location = "kwitter_page.html";
+}
+
+
+function getData() {
+      firebase.database().ref("/").on('value', function (snapshot) {
+            document.getElementById("output").innerHTML = ""; snapshot.forEach(function (childSnapshot) {
+                  childKey = childSnapshot.key;
+                  Room_names = childKey;
+                  //Start code
+                  console.log(Room_names);
+                  row = "<div class = 'room_name' id = " + Room_names + "onclick = 'redirectToRoomName(this.id)'>#" + Room_names + "</div> <hr>";
+                  document.getElementById("output").innerHTML += row;
+
+
+                  //End code
+            });
+      });
+}
+getData();
+
+function redirectToRoomName(name){
+console.log(name);
+localStorage.setItem("room_name", name);
+window.location = "kwitter_page.html";
+}
+function logout(){
+      localStorage.removeItem("user_name");
+      localStorage.removeItem("room_name");
+      window.location = "index.html";
+}
+
